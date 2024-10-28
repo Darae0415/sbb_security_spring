b09b063fd192d25affe54279d100ffec3fef95e9
Darae0415 <ekfogksmf@naver.com>
2024-10-28 09:38:59 +0900

    프로젝트 초기설정

---
+++ src/main/java/com/mysite/DemoSecurity/DemoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.DemoSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+}
